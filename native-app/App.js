668ba7b5584d9ca0ea9ac27ab08ea9bed7e4437a
Sonali Parekh <sonaliparekh@Sonalis-MacBook-Air.local>
2020-03-12 22:43:50 +0530

    course goals added

--- native-app/App.js
+++ native-app/App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [outputText, setoutputText] = useState("welcome to react");
   return (
-    <View style={styles.container}>
-      <Text>Welcome to React</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.inputText} />
+        <Button title="Add" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputText: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%"
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  }
 });
